8761c0738055780af81a5d2879ffe332e235f1c3
kashrocks <31697626+kashrocks@users.noreply.github.com>
2018-01-17 23:31:02 -0500

    Update sketch.js

--- Kashyap/sketch.js
+++ Kashyap/sketch.js
@@ -1,7 +1,7 @@
 
 var txt;
 var counts = {}; //value of object
-
+var keys = []; //stores pairs of unique words
 //loads the text file before hand
 function preload() {
 	//find the text file within the folder
@@ -20,18 +20,22 @@ function setup() {
 	console.log(tokens);
 	//loops through words looking for new words
 	for (var i =0; i< tokens.length; i++) {
-		var word = tokens[i];
+		var word = tokens[i].toLowerCase(); //converts all to lower case
 		//doesnt allow numbers
 		if(!/\d+/.test(word)){
 			if (counts[word] === undefined) {
 				counts[word] = 1; 
+				keys.push(word); 
 			} else {
 				counts[word]+= 1;
 			}
 		}
 	}
 
-
+	for (var i = 0; i < keys.leght; i++) {
+		var key = keys[i];
+		createDiv(key + " " + counts[key]);
+	}
 
 
 	noCanvas();
