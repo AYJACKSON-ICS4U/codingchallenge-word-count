a445ee8165deb3b4cf227bad699c9dd169d17153
kashrocks <31697626+kashrocks@users.noreply.github.com>
2018-01-17 22:47:22 -0500

    Update sketch.js

--- Kashyap/sketch.js
+++ Kashyap/sketch.js
@@ -6,9 +6,20 @@ function preload() {
 	//find the text file within the folder
 	txt = loadStrings('sample.txt');
 
+
+
 }
 
 function setup() {
+
+	//Joins all the words into one long word
+	var allwords = txt.join("\n");
+	//sperates each token into sperate (splits by all spaces and punctiation)
+	var tokens = allwords.split(/\W+/);
+	console.log("tokens");
+
+
+
 
 	noCanvas();
 }
